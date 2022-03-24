47c6ed0618884a4aa5953293e180d5534bdd67b3
Juan D Chaves <juandavidchaves@protonmail.com>
2022-03-24 20:26:42 -0300

    refactor

--- 01/script.js
+++ 01/script.js
@@ -17,8 +17,7 @@ function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = "form-control success";
 }
-
-
+ 
 // Check email is valid 
 
 function isValidEmail(email) {
@@ -27,35 +26,27 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+// Check required Fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === "") {
+            console.log(input.id);
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// Get fieldName
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event Listeners 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    if(username.value === "") {
-        showError(username, "Username is required")
-    } else {
-        showSuccess(username);
-    }
-
-    if(email.value === "") {
-        showError(email, "Email is required")
-    } else if (!isValidEmail(email.value)) {
-        showError(email, "Email is not valid")
-    } else {
-        showSuccess(email);
-    }
-
-    if(password.value === "") {
-        showError(password, "Password is required")
-    } else {
-        showSuccess(password);
+    checkRequired([username, email, password, password2]);
     
-    }
-
-    if(password2.value === "") {
-        showError(password2, "Password confirmation is required")
-    } else {
-        showSuccess(password2);
-    
-    }
-})
+});
