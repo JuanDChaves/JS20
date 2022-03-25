93aa56f9ad010c39e12bcc29c6e9b579773c8dea
Juan D Chaves <juandavidchaves@protonmail.com>
2022-03-25 17:42:25 -0300

    check length added

--- 01/script.js
+++ 01/script.js
@@ -20,10 +20,14 @@ function showSuccess(input) {
  
 // Check email is valid 
 
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value)) {
+        showSuccess(input);
+    } else {
+        showError(input, "Email is not valid");
+    }
 }
 
 // Check required Fields
@@ -38,6 +42,26 @@ function checkRequired(inputArr) {
     });
 }
 
+// Check input length
+
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Check password match
+
+function checkPasswordMatch(input1, input2) {
+    if(input1.value !== input2.value) {
+        showError(input2, "Passwords do not match");
+    } 
+}
+
 // Get fieldName
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -48,5 +72,8 @@ form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
-    
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
 });
