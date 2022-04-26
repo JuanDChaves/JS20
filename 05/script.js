411e7c63b3c17368d3115984fd48688124ae8a1d
Juan D Chaves <juandavidchaves@protonmail.com>
2022-04-26 14:50:26 -0300

    arr methods added

--- 05/script.js
+++ 05/script.js
@@ -27,6 +27,37 @@ async function getRandomUser() {
     addData(newUser);
 }
 
+// Double everyones money 
+function doubleMoney() {
+    data = data.map(user => {
+        return  { ...user, money: user.money * 2 };
+    });
+
+    updateDOM();
+}
+
+// Sort users by richest
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+}
+
+// Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => acc += user.money, 0);
+    const wealthEl = document.createElement("div");
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 function addData(obj) {
     data.push(obj);
@@ -34,11 +65,12 @@ function addData(obj) {
     updateDOM();
 }
 
+
 // Update DOM
 function updateDOM(providedData = data) {
     // Clear main div
-    main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`;
-
+    main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+    
     providedData.forEach(item => {
         const element = document.createElement("div");
         element.classList.add("person");
@@ -54,3 +86,7 @@ function formatMoney(number) {
 
 // Event Listeners
 addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
